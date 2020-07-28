102e676a864f00a0c515deb9c7dd8a69c75b2de2
detectrecog <detectrecog@gmail.com>
2020-07-28 11:54:09 +0800

    initial

--- config_mots/__init__.py
+++
@@ -1,5 +0,0 @@
-import config_mots.car_test_se_to_save
-import config_mots.car_test_tracking_val
-import config_mots.car_finetune_tracking
-import config_mots.car_finetune_SE_crop
-import config_mots.car_finetune_SE_mots
